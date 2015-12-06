af848bac8a48c07fee32518ca5c105baa8f19101
sisikopf <dimko12@gmail.com>
2015-12-06 13:02:22 +0300

    basic interfaces in quest-editor-api added

---
+++ quest-editor/quest-editor-api/src/main/java/org/textadventure/questeditor/questeditorapi/Item.java
@@ -0,0 +1,14 @@
+package org.textadventure.questeditor.questeditorapi;
+
+import java.util.Iterator;
+
+/**
+ * Created by dimko_000 on 06.12.2015.
+ */
+public interface Item {
+    String getName();
+    void setName(String name);
+    Iterator<Action> getActions();
+    void addAction(Action action);
+    void removeAction(Action action);
+}
