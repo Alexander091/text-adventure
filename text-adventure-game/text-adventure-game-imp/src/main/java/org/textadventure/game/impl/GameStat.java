e2e050cfc88db1b350c0a4ccb02593cc35ae85c9
Alex Alexanders <Sunny.Alexander@mail.ru>
2015-12-25 17:29:37 +0300

    Game-impl pom fixed

--- text-adventure-game/text-adventure-game-imp/src/main/java/org/textadventure/game/impl/GameStat.java
+++ text-adventure-game/text-adventure-game-imp/src/main/java/org/textadventure/game/impl/GameStat.java
@@ -1,16 +1,13 @@
 package org.textadventure.game.impl;
 
-import org.my.adventure.dao_manager.api.entities.Quest;
-import org.my.adventure.dao_manager.api.entities.Stat;
+
+import org.textadventure.questeditor.api.Stat;
 
 /**
  * Created by Alexander on 25.12.2015.
  */
 public class GameStat implements Stat {
-    @Override
-    public String getName() {
-        return null;
-    }
+
 
     @Override
     public void setName(String name) {
@@ -18,42 +15,17 @@ public class GameStat implements Stat {
     }
 
     @Override
-    public Float getValue() {
+    public String getName() {
         return null;
     }
 
     @Override
-    public void setValue(Float value) {
+    public void setValue(double value) {
 
     }
 
     @Override
-    public String getDescription() {
-        return null;
-    }
-
-    @Override
-    public void setDescription(String description) {
-
-    }
-
-    @Override
-    public Quest getQuestByQuestId() {
-        return null;
-    }
-
-    @Override
-    public void setQuestByQuestId(Quest questByQuestId) {
-
-    }
-
-    @Override
-    public Long getId() {
-        return null;
-    }
-
-    @Override
-    public void setId(Long id) {
-
+    public double getValue() {
+        return 0;
     }
 }
