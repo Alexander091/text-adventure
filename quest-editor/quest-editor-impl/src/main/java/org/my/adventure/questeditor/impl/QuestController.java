48cea4a6ffb79e410b0a534e7c0384c4214f58a8
sisikopf <dimko12@gmail.com>
2015-12-30 00:07:14 +0300

    quest settings tab added

--- quest-editor/quest-editor-impl/src/main/java/org/my/adventure/questeditor/impl/QuestController.java
+++ quest-editor/quest-editor-impl/src/main/java/org/my/adventure/questeditor/impl/QuestController.java
@@ -1,8 +1,30 @@
 package org.my.adventure.questeditor.impl;
 
+import org.my.adventure.dao_manager.api.dao.QuestDAO;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
 /**
  * Created by dimko_000 on 10.12.2015.
  */
+@ManagedBean(name="questBean")
 public class QuestController {
-   // QuestDao questDao;
+    QuestDAO questDAO;
+    public String getName() {
+        return "Test Quest";
+    }
+    public String getDescription() {
+        return "Some words about quest...";
+    }
+    public String[] getAllGenres() {
+        return new String[]{"Comedy", "Fantasy", "Horror"};
+    }
+    public int getVersion() {
+        return 1;
+    }
+    public String[] getAllAgeLimits() {
+        return new String[] {"0+","3+","6+","12+", "16+", "18+"};
+    }
 }
