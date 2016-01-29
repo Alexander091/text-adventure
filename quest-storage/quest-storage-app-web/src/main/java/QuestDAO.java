a74710238f50d9987a6fcfaaf1daae7d7da0b3ea
OctoOsmo <tatarincev_al@bk.ru>
2016-01-29 23:07:56 +0300

    quest list size was increased

---
+++ quest-storage/quest-storage-app-web/src/main/java/QuestDAO.java
@@ -0,0 +1,6 @@
+/**
+ * Created by al on 28.01.2016.
+ */
+public class QuestDAO {
+
+}
--- quest-storage/quest-storage-app-web/src/main/java/QuestStorageController.java
+++ quest-storage/quest-storage-app-web/src/main/java/QuestStorageController.java
@@ -16,6 +16,9 @@ public class QuestStorageController {
         quests.add(new Quest("Find the Queen!", "Detective", 87, QuestDifficulty.Hard));
         quests.add(new Quest("Escape from tower", "Detective", 64, QuestDifficulty.Medium));
         quests.add(new Quest("Dungeon master", "Fantasy", 96, QuestDifficulty.Easy));
+        quests.add(new Quest("Deeper", "RPG", 56, QuestDifficulty.Easy));
+        quests.add(new Quest("Awakenings", "Fantasy", 76, QuestDifficulty.Medium));
+        quests.add(new Quest("The Hitchhiker's Guide to the Galaxy", "Sci-fi", 72, QuestDifficulty.Hard));
     }
 
     public ArrayList<Quest> getQuests() {
@@ -25,4 +28,5 @@ public class QuestStorageController {
     public void setQuests(ArrayList<Quest> quests) {
         this.quests = quests;
     }
+
 }
