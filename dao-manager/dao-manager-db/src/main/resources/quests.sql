d394df61a00edda739a772524436245b11e28eb4
OctoOsmo <tatarincev_al@bk.ru>
2016-02-08 01:04:24 +0300

    sql scripts were updated

--- dao-manager/dao-manager-db/src/main/resources/quests.sql
+++ dao-manager/dao-manager-db/src/main/resources/quests.sql
@@ -1,6 +1,6 @@
 ﻿INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(123, 12, 'Первый тестовый квест, загруженный из базы данных',
-  'Фэнтези', 'Тестовый квест', 5.0, 1, NULL, 4);
+  'Фэнтези', 'Тестовый квест', 5.0, 1, NULL, NULL);
 
 INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(1, 12, 'Don''t Panic! Relax, because everything you need to know about playing The Hitchhiker''s Guide to the Galaxy is contained in the pages of this manual. In this story, you will be Arthur Dent, a rather ordinary earth creature who gets swept up in a whirlwind of interstellar adventures almost beyond comprehension. 
@@ -8,7 +8,7 @@ INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version
 As the story begins bulldozers are waiting to reduce your house to rubble to make way for a motorway bypass. While you attempt to deal with this problem, your rather strange friend Ford Prefect drops by to tell you that the Earth is about to be demolished to make way for an interstellar bypass! If you survive this double threat, you''ll embark on a series of inter-galactic misadventures even funnier than your worst nightmares! 
 
 A special note for people who have read the book "The Hitchhiker''s Guide to the Galaxy". Although the opening of the game is fairly similar to the book, the story quickly diverges, with lots of new material and different twists. Although familiarity with the story may make a few of the early puzzles easier, if you rely too heavily on this previous knowledge you will certainly end up getting misled.',
-  'Фэнтези', 'The Hitchhiker''s Guide to the Galaxy', 75.0, 1, NULL, 2);
+  'Фэнтези', 'The Hitchhiker''s Guide to the Galaxy', 75.0, 1, NULL, NULL);
 
 INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(2, 12, 'You''re a great detective living in Victorian London. Your internal monologue will guide you by clicking on links in the body of text as you investigate a seemingly average mugging. 
@@ -20,24 +20,45 @@ Your intensely fast analyzing ability is represented by your unlimited time betw
 Your vast knowledge is represented by the internet... 
 
 London needs you!',
-  'Детектив', 'Victorian detective', 87.0, 1, NULL, 3);
+  'Детектив', 'Victorian detective', 87.0, 1, NULL, NULL);
 
   INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(3, 14, 'A half-true game about half-truths. You play as a semi-fictional version of me, on a night that changed my life forever. Choose your (my?) words wisely. Every character will remember everything you say - or don''t say - as you figure out how to approach my (your?) hyper-conservative Asian parents. And if all that seems confusing or awkward... well, that''s the gist of coming out as queer, isn''t it? ',
-  'Slice of life', 'Coming Out Simulator', 25.4, 1, NULL, 1);
+  'Slice of life', 'Coming Out Simulator', 25.4, 1, NULL, NULL);
 
   INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(4, 12, 'Найдите королеву!',
-  'Фэнтези', 'Find the queen!', 42.0, 2, NULL, 4);
+  'Фэнтези', 'Find the queen!', 42.0, 2, NULL, NULL);
 
     INSERT INTO public.quest(id, agelimit, description, genre, name, rating, version, start_node, image_resource)
  VALUES(5, 12, 'Совершите дерзкий побег!',
-  'Фэнтези', 'Escape from tower', 42.0, 2, NULL, 4);
-
-
+  'Фэнтези', 'Escape from tower', 42.0, 2, NULL, NULL);
 
 INSERT INTO resource (id, name, path, quest_id, type_id) VALUES (1, 'coming out simulator 2014', '/resources/images/coming_out.png', 3, 1);
 INSERT INTO resource (id, name, path, quest_id, type_id) VALUES (2, 'Hitchhiker', '/resources/images/Hitchhiker.jpg', 1, 1);
 INSERT INTO resource (id, name, path, quest_id, type_id) VALUES (3, 'Victorian_detective', '/resources/images/Victorian_detective.jpg', 2, 1);
 INSERT INTO resource (id, name, path, quest_id, type_id) VALUES (4, 'Placeholder', '/resources/images/arrow.png', 123, 1);
-  
+
+UPDATE quest
+   SET image_resource=4
+ WHERE id = 123;
+
+ UPDATE quest
+   SET image_resource=2
+ WHERE id = 1;
+
+ UPDATE quest
+   SET image_resource=3
+ WHERE id = 2;
+
+ UPDATE quest
+   SET image_resource=1
+ WHERE id = 3;
+
+ UPDATE quest
+   SET image_resource=4
+ WHERE id = 4;
+
+ UPDATE quest
+   SET image_resource=4
+ WHERE id = 5;
--- dao-manager/dao-manager-db/src/main/resources/testdata.sql
+++ dao-manager/dao-manager-db/src/main/resources/testdata.sql
@@ -1,43 +1,41 @@
 INSERT INTO public.quest(id, agelimit, description, genre, name,
 rating, version, start_node)
- VALUES(123, 12, 'Первый тестовый квест, загруженный из базы данных',
-  'Фэнтези', 'Тестовый квест', 5.0, 1, NULL);
-
-
+ VALUES(123, 12, 'РџРµСЂРІС‹Р№ С‚РµСЃС‚РѕРІС‹Р№ РєРІРµСЃС‚, Р·Р°РіСЂСѓР¶РµРЅРЅС‹Р№ РёР· Р±Р°Р·С‹ РґР°РЅРЅС‹С…',
+  'Р¤СЌРЅС‚РµР·Рё', 'РўРµСЃС‚РѕРІС‹Р№ РєРІРµСЃС‚', 5.0, 1, NULL);
 
 INSERT INTO public.node(id, description, name, quest_id, position)
- VALUES(1, 'Дом, милый дом',
-  'Дом', 123, '250 125');
+ VALUES(1, 'Р”РѕРј, РјРёР»С‹Р№ РґРѕРј',
+  'Р”РѕРј', 123, '250 125');
 
 INSERT INTO public.node(id, description, name, quest_id, position)
- VALUES(2, 'Странные звуки доносятся из этого леса ночью...',
-  'Лес', 123, '125 250');
+ VALUES(2, 'РЎС‚СЂР°РЅРЅС‹Рµ Р·РІСѓРєРё РґРѕРЅРѕСЃСЏС‚СЃСЏ РёР· СЌС‚РѕРіРѕ Р»РµСЃР° РЅРѕС‡СЊСЋ...',
+  'Р›РµСЃ', 123, '125 250');
 
  INSERT INTO public.node(id, description, name, quest_id, position)
- VALUES(3, 'Немало странников пропало на этой дороге',
-  'Дорога', 123, '375 250');
+ VALUES(3, 'РќРµРјР°Р»Рѕ СЃС‚СЂР°РЅРЅРёРєРѕРІ РїСЂРѕРїР°Р»Рѕ РЅР° СЌС‚РѕР№ РґРѕСЂРѕРіРµ',
+  'Р”РѕСЂРѕРіР°', 123, '375 250');
 
  INSERT INTO public.node(id, description, name, quest_id, position)
- VALUES(4, 'Вы прошли все трудности и победили! Поздравляем!',
-  'Финал', 123, '250 375');
+ VALUES(4, 'Р’С‹ РїСЂРѕС€Р»Рё РІСЃРµ С‚СЂСѓРґРЅРѕСЃС‚Рё Рё РїРѕР±РµРґРёР»Рё! РџРѕР·РґСЂР°РІР»СЏРµРј!',
+  'Р¤РёРЅР°Р»', 123, '250 375');
 
  INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(1, 'Тест', 'Пройтись в лес', 1, 2);
+ VALUES(1, 'РўРµСЃС‚', 'РџСЂРѕР№С‚РёСЃСЊ РІ Р»РµСЃ', 1, 2);
 
   INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(2, 'Тест', 'Идти к дороге', 1, 3);
+ VALUES(2, 'РўРµСЃС‚', 'РРґС‚Рё Рє РґРѕСЂРѕРіРµ', 1, 3);
 
   INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(3, 'Тест', 'Перейти к финалу', 2, 4);
+ VALUES(3, 'РўРµСЃС‚', 'РџРµСЂРµР№С‚Рё Рє С„РёРЅР°Р»Сѓ', 2, 4);
 
    INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(4, 'Тест', 'Перейти к финалу', 3, 4);
+ VALUES(4, 'РўРµСЃС‚', 'РџРµСЂРµР№С‚Рё Рє С„РёРЅР°Р»Сѓ', 3, 4);
 
    INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(5, 'Тест', 'Выйти из леса к дороге', 2, 3);
+ VALUES(5, 'РўРµСЃС‚', 'Р’С‹Р№С‚Рё РёР· Р»РµСЃР° Рє РґРѕСЂРѕРіРµ', 2, 3);
 
    INSERT INTO public.transition(id, condition, name, from_node, to_node)
- VALUES(6, 'Тест', 'Вернуться в дом', 3, 1);
+ VALUES(6, 'РўРµСЃС‚', 'Р’РµСЂРЅСѓС‚СЊСЃСЏ РІ РґРѕРј', 3, 1);
 
 UPDATE public.quest
 SET start_node = 1
