2f6896b5863e17c0e19c9eec7dbb507c1f533fd9
OctoOsmo <tatarincev_al@bk.ru>
2016-03-06 03:08:24 +0300

    problem with quest id in Upload controlle has been solved

--- quest-editor/quest-editor-web/src/main/java/org/my/adventure/questeditor/web/jsf/UploadController.java
+++ quest-editor/quest-editor-web/src/main/java/org/my/adventure/questeditor/web/jsf/UploadController.java
@@ -1,20 +1,16 @@
 package org.my.adventure.questeditor.web.jsf;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.my.adventure.questeditor.ejb.beans.ResourceUploaderBean;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
-import sun.misc.IOUtils;
-import sun.nio.ch.IOUtil;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,10 +18,21 @@ import java.io.InputStream;
  * Created by dimko_000 on 11.02.2016.
  */
 @ManagedBean(name="uploadController")
+@ViewScoped
 public class UploadController {
 
     @EJB
     private ResourceUploaderBean uploaderBean;
+
+    Long questId;
+
+    public Long getQuestId() {
+        return questId;
+    }
+
+    public void setQuestId(Long questId) {
+        this.questId = questId;
+    }
 
     public void upload(FileUploadEvent event) {
         UploadedFile file = event.getFile();
@@ -37,10 +44,6 @@ public class UploadController {
             catch (IOException e) {
                 e.printStackTrace();
             }
-            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-            String questIdStr = externalContext.getRequestParameterMap().get("editorForm:questIdInput");
-//            Long questId = Long.parseLong(questIdStr);
-            Long questId = null;
             String fileExtension = FilenameUtils.getExtension(file.getFileName());
             Long typeId = null;
             if(fileExtension.equals("jpg")||fileExtension.equals("png")) {
@@ -53,6 +56,5 @@ public class UploadController {
             FacesMessage message = new FacesMessage("Файл", event.getFile().getFileName() + " Загружен.");
             FacesContext.getCurrentInstance().addMessage(null,message);
         }
-
     }
 }
